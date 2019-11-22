a5af61869a7cf6f5b0dcc05da287fb5c3b8345d3
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-11-22 18:25:59 +0100

    Update moviedb.py

--- moviedb.py
+++ moviedb.py
@@ -19,33 +19,42 @@ def create(file):
 	tmp = {}
 
 	try:
-		with open(args.file_in) as f
+		with open(args.file_in) as f:
+			for line in f:
+				tmp = json.loads(line)
+
+				if ("year" in tmp) and ("directors" in tmp):			
+					for tmp_act in tmp["cast"]:
+						if tmp_act not in actors:
+							actors[tmp_act] = [(tmp['title'], tmp['year'])]
+						else:
+							actors[tmp_act].append((tmp['title'], tmp['year']))
+
+					for tmp_directors in tmp["directors"]:
+						if tmp_directors not in directors:
+							directors[tmp_directors] = [(tmp['title'], tmp['year'])]
+						else:
+							directors[tmp_directors].append((tmp['title'], tmp['year']))
+
 	except(FileNotFoundError, IOError):
 		print("Le fichier spécifié n'existe pas ou le chemin d'accès est erronné.")
-		raise
 		exit
 	else:	
-		for line in f:
-			tmp = json.loads(line)
 			
-			if ("year" in tmp) and ("directors" in tmp):			
-				for tmp_act in tmp["cast"]:
-					if tmp_act not in actors:
-						actors[tmp_act] = [(tmp['title'], tmp['year'])]
-					else:
-						actors[tmp_act].append((tmp['title'], tmp['year']))
-				
-				for tmp_directors in tmp["directors"]:
-					if tmp_directors not in directors:
-						directors[tmp_directors] = [(tmp['title'], tmp['year'])]
-					else:
-						directors[tmp_directors].append((tmp['title'], tmp['year']))		
-	
-	with open('directors.json', 'w',  encoding="utf8") as d: # on crée un fichier json appelé directors.json
-		json.dump(directors, d) # dans lequel on y copie le dictionnaire "directors"
-	
-	with open('actors.json', 'w', encoding='utf8') as a: # # on crée un fichier json appelé actors.json
-		json.dump(actors, a) # dans lequel on y copie le dictionnaire "actors"
+		try:
+			with open('directors.json', 'w',  encoding="utf8") as d: # on crée un fichier json appelé directors.json
+				json.dump(directors, d) # dans lequel on y copie le dictionnaire "directors"
+		except(PermissionError):
+			print("Le programme n'a pas pu s'exécuter.")
+			#raise
+			exit
+		try:
+			with open('actors.json', 'w', encoding='utf8') as a:  # on crée un fichier json appelé actors.json
+				json.dump(actors, a) # dans lequel on y copie le dictionnaire "actors"
+		except(PermissionError):
+			print("Le programme n'a pas pu s'exécuter.")
+			#raise
+			exit
 
 # ======================================================================================================= #
 
@@ -76,25 +85,36 @@ def actor(actor_input):
 		print("Le fichier spécifié n'existe pas ou le chemin d'accès est erronné.")
 
 	if actor_input not in acteur_choisi:
-		with open(csv_file, 'w' ,newline='') as csvfile:
-			csvfile.write("none")
+		try:
+			with open(csv_file, 'w' ,newline='') as csvfile:
+				csvfile.write("none")
+		except(PermissionError):
+			print("Le programme n'a pas pu s'exécuter.")
+			raise
+	
 	else:
+		# print(acteur_choisi)
+
 		for film, annee in acteur_choisi[actor_input]:
 			if annee in nbr_films_annee:
 				nbr_films_annee[annee] += 1
 			else:
 				nbr_films_annee[annee] = 1
 
-		#ordonnee_nbr_films_annee = sorted(nbr_films_annee.items(), key=operator.itemgetter(0))
-		#print(ordonnee_nbr_films_annee)
+		# ordonnee_nbr_films_annee = sorted(nbr_films_annee.items(), key=operator.itemgetter(0))
+		# print(ordonnee_nbr_films_annee)
 
-		print(nbr_films_annee)
+		# print(nbr_films_annee)
 
-		with open(csv_file, 'w' ,newline='') as csvfile:
-			writer = csv.writer(csvfile)
-			writer.writerow(["Année", "NbFilms"])
-			for annee, nbfilms in nbr_films_annee.items():
-				writer.writerow([annee, nbfilms])
+		try:
+			with open(csv_file, 'w' ,newline='') as csvfile:
+				writer = csv.writer(csvfile)
+				writer.writerow(["Année", "NbFilms"])
+				for annee, nbfilms in nbr_films_annee.items():
+					writer.writerow([annee, nbfilms])
+		except(PermissionError):
+			print("Le programme n'a pas pu écrire les nouveaux fichiers.")
+			raise			
 
 # ======================================================================================================= #
 
@@ -140,7 +160,6 @@ def director(director_input):
 		except(PermissionError):
 			print("Le programme n'a pas pu s'exécuter.")
 			raise
-			exit
 	finally:
 		print("Fin de l'exécution de la fonction.")
 

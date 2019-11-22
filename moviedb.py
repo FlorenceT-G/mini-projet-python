a301873cbbda5c0e37e120c5d1afaae184bd543e
FlorenceT-G <57944788+FlorenceT-G@users.noreply.github.com>
2019-11-22 18:08:46 +0100

    Update moviedb.py

--- moviedb.py
+++ moviedb.py
@@ -17,8 +17,14 @@ def create(file):
 	directors = collections.defaultdict()
 	
 	tmp = {}
-	
-	with open(args.file_in) as f:
+
+	try:
+		with open(args.file_in) as f
+	except(FileNotFoundError, IOError):
+		print("Le fichier spécifié n'existe pas ou le chemin d'accès est erronné.")
+		raise
+		exit
+	else:	
 		for line in f:
 			tmp = json.loads(line)
 			
@@ -33,7 +39,7 @@ def create(file):
 					if tmp_directors not in directors:
 						directors[tmp_directors] = [(tmp['title'], tmp['year'])]
 					else:
-						directors[tmp_directors].append((tmp['title'], tmp['year']))
+						directors[tmp_directors].append((tmp['title'], tmp['year']))		
 	
 	with open('directors.json', 'w',  encoding="utf8") as d: # on crée un fichier json appelé directors.json
 		json.dump(directors, d) # dans lequel on y copie le dictionnaire "directors"
@@ -41,49 +47,108 @@ def create(file):
 	with open('actors.json', 'w', encoding='utf8') as a: # # on crée un fichier json appelé actors.json
 		json.dump(actors, a) # dans lequel on y copie le dictionnaire "actors"
 
+# ======================================================================================================= #
+
 def actor(actor_input):
 	# créé une liste au format CSV à partir de actors.json recensant le nombre de films par année
 	# year, nbfilm
 	# 1987; 2
 	# ...
 
-	print(actor_input)
+	#print(actor_input)
+
 	actor_choisi = {}
 	nbr_films_annee = {}
 	csv_columns=["Année", "Nombre de films"]
 
-	with open("actors.json") as f:
-		for line in f:
-			acteur_choisi = json.loads(line)
+	prenom_nom = actor_input.split() # creation d'une liste contenant le nom et le prénom de l'acteur passé en argument
 
-	for film, annee in acteur_choisi[actor_input]:
-		if annee in nbr_films_annee:
-			nbr_films_annee[annee] += 1
+	prenom = prenom_nom[0] # stocke le prénom dans une variable
+	nom = prenom_nom[1] # strocke le nom dans une autre vrariable
+
+	csv_file = prenom + "_" + nom + ".csv" # concaténation afin d'obtenir un modèle : Prénom_Nom.csv
+
+	try:
+		with open("actors.json") as f:
+			for line in f:
+				acteur_choisi = json.loads(line)
+	except(FileNotFoundError, IOError):
+		print("Le fichier spécifié n'existe pas ou le chemin d'accès est erronné.")
+
+	if actor_input not in acteur_choisi:
+		with open(csv_file, 'w' ,newline='') as csvfile:
+			csvfile.write("none")
+	else:
+		for film, annee in acteur_choisi[actor_input]:
+			if annee in nbr_films_annee:
+				nbr_films_annee[annee] += 1
+			else:
+				nbr_films_annee[annee] = 1
+
+		#ordonnee_nbr_films_annee = sorted(nbr_films_annee.items(), key=operator.itemgetter(0))
+		#print(ordonnee_nbr_films_annee)
+
+		print(nbr_films_annee)
+
+		with open(csv_file, 'w' ,newline='') as csvfile:
+			writer = csv.writer(csvfile)
+			writer.writerow(["Année", "NbFilms"])
+			for annee, nbfilms in nbr_films_annee.items():
+				writer.writerow([annee, nbfilms])
+
+# ======================================================================================================= #
+
+def director(director_input):
+
+	directeur_choisi = {}
+	nbr_film_annee = {}
+
+	prenom_nom = director_input.split()
+	prenom = prenom_nom[0] ; nom = prenom_nom[1]
+	csv_file = prenom + "_" + nom + ".csv"
+
+
+	try:
+		with open("directors.json") as f:
+			for line in f:
+				directeur_choisi = json.loads(line)
+	except(FileNotFoundError, IOError):
+		print("Le fichier spécifié n'existe pas ou le chemin d'accès est erronné.")
+		raise
+		exit
+	else:
+		if director_input not in directeur_choisi:
+			with open(csv_file, 'w' ,newline='') as csvfile:
+				csvfile.write("none")
 		else:
-			nbr_films_annee[annee] = 1
-	
-	prenom_nom = actor_input.split()
+			for film, annee in directeur_choisi[director_input]:
+				if annee in nbr_films_annee:
+					nbr_films_annee[annee] += 1
+				else:
+					nbr_films_annee[annee] = 1
 
-	prenom = prenom_nom[0]
-	nom = prenom_nom[1]
+		# ordonnee_nbr_films_annee = sorted(nbr_films_annee.items(), key=operator.itemgetter(0))
+		# print(ordonnee_nbr_films_annee)
+		# print(nbr_films_annee)
 
-	ordonnee_nbr_films_annee = sorted(nbr_films_annee.items(), key=operator.itemgetter(0))
-	print(ordonnee_nbr_films_annee)
+		try:
+			with open(csv_file, 'w' ,newline='') as csvfile:
+				writer = csv.writer(csvfile)
+				writer.writerow(["Année", "NbFilms"])
+				for annee, nbfilms in nbr_films_annee.items():
+					writer.writerow([annee, nbfilms])
+		except(PermissionError):
+			print("Le programme n'a pas pu s'exécuter.")
+			raise
+			exit
+	finally:
+		print("Fin de l'exécution de la fonction.")
 
-	csv_file = actor_input + ".csv"
-
-	with open(csv_file, 'w' ,newline='') as csvfile:
-		writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-		writer.writeheader()
-		for films in ordonnee_nbr_films_annee:
-			writer.writerow(films)
-			
-'''def director():
-	# même chose que pour acteur(), à partir de directors.json'''
+# ======================================================================================================= #
 
 if __name__ == "__main__":
 
-	print(len(sys.argv))
+	# print(len(sys.argv))
 	if(sys.argv == 1):
 		print("Erreur, aucun arguments.")
 	else:
